241cef99921d31b3686b0457c0f7fbae8daedbe1
Felix Cramer <felixcramer18@gmail.com>
2023-02-22 15:02:16 +0100

    second push

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :category, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :category, inclusion: %w"[ chinese italian japanese french belgian ]"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_103907) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.text "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,8 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_103907) do
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
